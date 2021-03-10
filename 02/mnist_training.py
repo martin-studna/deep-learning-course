55e11580d2186403bd5f2ac4d8a522436c39cf65
Martin studna <martin.studna2@gmail.com>
2021-03-10 10:12:57 +0100

    feat: init

--- 02/mnist_training.py
+++ 02/mnist_training.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
+from mnist import MNIST
+import tensorflow as tf
+import numpy as np
 import argparse
 import datetime
 import os
 import re
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2") # Report only TF errors by default
+# Report only TF errors by default
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")
 
-import numpy as np
-import tensorflow as tf
-
-from mnist import MNIST
 
 # 2f67b427-a885-11e7-a937-00505601122b
 # c751264b-78ee-11eb-a1a9-005056ad4f31
@@ -16,17 +16,25 @@ from mnist import MNIST
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
-parser.add_argument("--decay", default=None, type=str, help="Learning decay rate type")
+parser.add_argument("--decay", default=None, type=str,
+                    help="Learning decay rate type")
 parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
-parser.add_argument("--hidden_layer", default=200, type=int, help="Size of the hidden layer.")
-parser.add_argument("--learning_rate", default=0.01, type=float, help="Initial learning rate.")
-parser.add_argument("--learning_rate_final", default=None, type=float, help="Final learning rate.")
+parser.add_argument("--hidden_layer", default=200, type=int,
+                    help="Size of the hidden layer.")
+parser.add_argument("--learning_rate", default=0.01,
+                    type=float, help="Initial learning rate.")
+parser.add_argument("--learning_rate_final", default=None,
+                    type=float, help="Final learning rate.")
 parser.add_argument("--momentum", default=None, type=float, help="Momentum.")
-parser.add_argument("--optimizer", default="SGD", type=str, help="Optimizer to use.")
-parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
+parser.add_argument("--optimizer", default="SGD",
+                    type=str, help="Optimizer to use.")
+parser.add_argument("--recodex", default=False,
+                    action="store_true", help="Evaluation in ReCodEx.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--threads", default=1, type=int,
+                    help="Maximum number of threads to use.")
 # If you add more arguments, ReCodEx will keep them with your default values.
+
 
 def main(args):
     # Fix random seeds and threads
@@ -35,15 +43,19 @@ def main(args):
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
     if args.recodex:
-        tf.keras.utils.get_custom_objects()["glorot_uniform"] = tf.initializers.GlorotUniform(seed=args.seed)
-        tf.keras.utils.get_custom_objects()["orthogonal"] = tf.initializers.Orthogonal(seed=args.seed)
-        tf.keras.utils.get_custom_objects()["uniform"] = tf.initializers.RandomUniform(seed=args.seed)
+        tf.keras.utils.get_custom_objects(
+        )["glorot_uniform"] = tf.initializers.GlorotUniform(seed=args.seed)
+        tf.keras.utils.get_custom_objects(
+        )["orthogonal"] = tf.initializers.Orthogonal(seed=args.seed)
+        tf.keras.utils.get_custom_objects(
+        )["uniform"] = tf.initializers.RandomUniform(seed=args.seed)
 
     # Create logdir name
     args.logdir = os.path.join("logs", "{}-{}-{}".format(
         os.path.basename(globals().get("__file__", "notebook")),
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+        ",".join(("{}={}".format(re.sub(
+            "(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
 
     # Load data
@@ -79,14 +91,35 @@ def main(args):
     #   rate manually by using `model.optimizer.learning_rate(model.optimizer.iterations)`,
     #   so after training this value should be `args.learning_rate_final`.
 
+    decay = None
+
+    if args.decay is not None:
+        if args.decay == "polynomial":
+            decay = tf.keras.optimizers.schedules.PolynomialDecay(
+                args.learning_rate, mnist.train.size / args.batch_size, end_learning_rate=args.learning_rate_final)
+        elif args.decay == "exponential":
+            decay = tf.keras.optimizers.ExponentialDecay(
+                args.learning_rate, mnist.train.size / args.batch_size, 0.002)
+
+    optimizer = None
+
+    if args.optimizer == "SGD":
+        optimizer = tf.keras.optimizers.SGD(
+            lr=args.learning_rate, momentum=args.momentum, decay=decay)
+    elif args.optimizer == "Adam":
+        optimizer = tf.keras.optimizers.Adam(
+            lr=args.learning_rate, decay=decay)
+
     model.compile(
-        optimizer=...,
+        optimizer=args.optimizer,
         loss=tf.losses.SparseCategoricalCrossentropy(),
         metrics=[tf.metrics.SparseCategoricalAccuracy("accuracy")],
     )
 
-    tb_callback = tf.keras.callbacks.TensorBoard(args.logdir, histogram_freq=1, update_freq=100, profile_batch=0)
-    tb_callback._close_writers = lambda: None # Ugly hack allowing to log also test data metrics.
+    tb_callback = tf.keras.callbacks.TensorBoard(
+        args.logdir, histogram_freq=1, update_freq=100, profile_batch=0)
+    # Ugly hack allowing to log also test data metrics.
+    tb_callback._close_writers = lambda: None
     model.fit(
         mnist.train.data["images"], mnist.train.data["labels"],
         batch_size=args.batch_size, epochs=args.epochs,
@@ -97,10 +130,12 @@ def main(args):
     test_logs = model.evaluate(
         mnist.test.data["images"], mnist.test.data["labels"], batch_size=args.batch_size, return_dict=True,
     )
-    tb_callback.on_epoch_end(args.epochs, {"val_test_" + metric: value for metric, value in test_logs.items()})
+    tb_callback.on_epoch_end(
+        args.epochs, {"val_test_" + metric: value for metric, value in test_logs.items()})
 
     # Return test accuracy for ReCodEx to validate
     return test_logs["accuracy"]
+
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
