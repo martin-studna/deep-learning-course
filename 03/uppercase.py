8cafb7323a5d863c44b743d26df2ab958de4ce7a
mysak <mysak@cloudcode.cz>
2021-03-29 17:49:12 +0200

    uppercase_ albert train ready

--- 03/uppercase.py
+++ 03/uppercase.py
@@ -1,71 +1,125 @@
 #!/usr/bin/env python3
+from uppercase_data import UppercaseData
+import tensorflow as tf
+import numpy as np
 import argparse
 import datetime
 import os
 import re
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2") # Report only TF errors by default
 
-import numpy as np
-import tensorflow as tf
+import neptune
 
-from uppercase_data import UppercaseData
+neptune.init(project_qualified_name='amdalifuk/c10', api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiZjkxYTA4NzYtMzk5OS00NDFmLTlmMWItNGNhNjU5NmQ1NDMxIn0=') # add your 
 
-# TODO: Set reasonable values for the hyperparameters, notably
-# for `alphabet_size` and `window` and others.
-parser = argparse.ArgumentParser()
-parser.add_argument("--alphabet_size", default=None, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
-parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
-parser.add_argument("--seed", default=42, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-parser.add_argument("--window", default=None, type=int, help="Window size to use.")
+# Report only TF errors by default
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")
 
-def main(args):
-    # Fix random seeds and threads
-    np.random.seed(args.seed)
-    tf.random.set_seed(args.seed)
-    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
-    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+np.random.seed(42)
+tf.random.set_seed(42)
+tf.config.threading.set_inter_op_parallelism_threads(4)
+tf.config.threading.set_intra_op_parallelism_threads(4)
 
-    # Create logdir name
-    args.logdir = os.path.join("logs", "{}-{}-{}".format(
-        os.path.basename(globals().get("__file__", "notebook")),
-        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
-    ))
+#hloubka a počet parametrů, dropout
+hidden_layers = [1600,1600]
+dropout = 0.2 #True/False
+l2 = 0 #0,0.1
+bn = True #True/False
+lr = 0.001
+window = 5
+alphabet_size = 100
+label_smoothing = 0.1
+loss = tf.losses.CategoricalCrossentropy( label_smoothing=label_smoothing) #?
+batch_size = 5000
+afunkce= 'relu'
+epochs = 40
 
-    # Load data
-    uppercase_data = UppercaseData(args.window, args.alphabet_size)
+uppercase_data = UppercaseData(window, alphabet_size)
+labels = tf.keras.utils.to_categorical(uppercase_data.train.data["labels"])
+labels_dev = tf.keras.utils.to_categorical(uppercase_data.dev.data["labels"])
 
-    # TODO: Implement a suitable model, optionally including regularization, select
-    # good hyperparameters and train the model.
-    #
-    # The inputs are _windows_ of fixed size (`args.window` characters on left,
-    # the character in question, and `args.window` characters on right), where
-    # each character is represented by a `tf.int32` index. To suitably represent
-    # the characters, you can:
-    # - Convert the character indices into _one-hot encoding_. There is no
-    #   explicit Keras layer, but you can
-    #   - use a Lambda layer which can encompass any function:
-    #       tf.keras.Sequential([
-    #         tf.keras.layers.InputLayer(input_shape=[2 * args.window + 1], dtype=tf.int32),
-    #         tf.keras.layers.Lambda(lambda x: tf.one_hot(x, len(uppercase_data.train.alphabet))),
-    #   - or use Functional API and then any TF function can be used
-    #     as a Keras layer:
-    #       inputs = tf.keras.layers.Input(shape=[2 * args.window + 1], dtype=tf.int32)
-    #       encoded = tf.one_hot(inputs, len(uppercase_data.train.alphabet))
-    #   You can then flatten the one-hot encoded windows and follow with a dense layer.
-    # - Alternatively, you can use `tf.keras.layers.Embedding` (which is an efficient
-    #   implementation of one-hot encoding followed by a Dense layer) and flatten afterwards.
-    model = ...
+def get_network():
+    
+    l1l2_regularizer = None
+    
+    if l2 != 0:
+        l1l2_regularizer = tf.keras.regularizers.L1L2(l1=0, l2=l2)
 
-    # TODO: Generate correctly capitalized test set.
-    # Use `uppercase_data.test.text` as input, capitalize suitable characters,
-    # and write the result to predictions_file (which is
-    # `uppercase_test.txt` in the `args.logdir` directory).
-    with open(os.path.join(args.logdir, "uppercase_test.txt"), "w", encoding="utf-8") as predictions_file:
-        ...
+    model = tf.keras.Sequential([
+        tf.keras.layers.InputLayer(
+            input_shape=[2 * window + 1], dtype=tf.int32),
+        tf.keras.layers.Lambda(lambda x: tf.one_hot(x, len(uppercase_data.train.alphabet)))])
+    
+    model.add(tf.keras.layers.Flatten())
+    
+    for hidden_layer in hidden_layers:
+        model.add(tf.keras.layers.Dense(hidden_layer,
+                  activation=afunkce, kernel_regularizer=l1l2_regularizer))
+        
+        if dropout != 0:
+            model.add(tf.keras.layers.Dropout(dropout))
+        
+        if bn:
+            model.add(tf.keras.layers.BatchNormalization() )
+            
+            
 
-if __name__ == "__main__":
-    args = parser.parse_args([] if "__file__" not in globals() else None)
-    main(args)
+    model.add(tf.keras.layers.Dense(2,
+              activation=tf.nn.softmax, kernel_regularizer=l1l2_regularizer))
+    return model
+    
+model = get_network()
+model.compile(
+        optimizer=tf.optimizers.Adam( lr ),
+        loss=loss,
+        metrics=[tf.metrics.CategoricalAccuracy(name="accuracy")],
+    )
+model.summary()
+
+
+PARAMS = {
+    'hidden_layers': hidden_layers,
+    'dropout': dropout,
+    'afunkce': afunkce,
+    'l2': l2,
+    'bn': bn,
+    'lr': lr,
+    'window': window,
+    'loss': loss,
+    'batch_size': batch_size,
+    'label_smoothing': label_smoothing,
+    'alphabet_size': alphabet_size,
+    'epochs': epochs,                 
+         }
+neptune.create_experiment(params=PARAMS,abort_callback=lambda: run_shutdown_logic_and_exit())
+neptune.send_artifact('uppercase.ipynb')
+
+#13 epoch
+from tensorflow.keras.callbacks import Callback
+class CustomCallback(Callback):        
+    def on_epoch_end(self, epoch, logs=None):
+
+        neptune.log_metric('loss', logs['loss'])
+        neptune.log_metric('1-accuracy', 1-logs['accuracy'])
+        
+        if 'val_loss' in logs:
+            neptune.log_metric('val_loss', logs['val_loss'])
+            neptune.log_metric('1-val_accuracy', 1-logs['val_accuracy'])
+            
+model.fit(uppercase_data.train.data["windows"], labels, batch_size=batch_size, shuffle=True,
+          validation_data=(uppercase_data.dev.data["windows"], labels_dev), epochs=epochs, callbacks=[CustomCallback()])
+
+
+predictions = model.predict(uppercase_data.test.data["windows"])
+text_result = list(uppercase_data.test.text)
+
+with open("input.txt", "w", encoding="utf-8") as predictions_file:
+    predictions_file.write(uppercase_data.test.text)
+
+for i in range(len(predictions)):
+    if predictions[i][1] >= 0.5:
+        if text_result[i] == text_result[i].upper().lower():
+            text_result[i] = text_result[i].upper()
+
+text_result = "".join(text_result)
+with open("uppercase_test.txt", "w", encoding="utf-8") as predictions_file:
+    predictions_file.write(text_result)
