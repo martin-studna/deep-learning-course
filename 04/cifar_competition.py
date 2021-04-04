0c1809414cda57978b14689445023efdc6c50977
mysak <mysak@cloudcode.cz>
2021-04-05 01:36:10 +0200

    bsize1,dropoutWL

--- 04/cifar_competition.py
+++ 04/cifar_competition.py
@@ -39,11 +39,11 @@ os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")
 
 # TODO: Define reasonable defaults and optionally more parameters
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=64*5, type=int, help="Batch size.")
+parser.add_argument("--batch_size", default=64*1, type=int, help="Batch size.")
 parser.add_argument("--learning_rate", default=0.001,
                     type=int, help="Batch size.")
 parser.add_argument("--momentum", default=0.9, type=float, help="Momentum.")
-parser.add_argument("--l2", default=0.0001, type=float,
+parser.add_argument("--l2", default=0.000, type=float,
                     help="L2 regularization.")
 parser.add_argument("--epochs", default=400,
                     type=int, help="Number of epochs.")
@@ -86,7 +86,7 @@ def main(args):
                      kernel_initializer='he_uniform', padding='same'))
     model.add(BatchNormalization())
     model.add(MaxPooling2D((2, 2)))
-    #model.add(Dropout(0.2))
+    model.add(Dropout(0.2))
     model.add(Conv2D(64//v, (3, 3), activation='relu', kernel_regularizer=l2(args.l2),
                      kernel_initializer='he_uniform', padding='same'))
     model.add(BatchNormalization())
@@ -94,7 +94,7 @@ def main(args):
                      kernel_initializer='he_uniform', padding='same'))
     model.add(BatchNormalization())
     model.add(MaxPooling2D((2, 2)))
-    #model.add(Dropout(0.3))
+    model.add(Dropout(0.3))
     model.add(Conv2D(128//v, (3, 3), activation='relu', kernel_regularizer=l2(args.l2),
                      kernel_initializer='he_uniform', padding='same'))
     model.add(BatchNormalization())
@@ -102,7 +102,7 @@ def main(args):
                      kernel_initializer='he_uniform', padding='same'))
     model.add(BatchNormalization())
     model.add(MaxPooling2D((2, 2)))
-    #model.add(Dropout(0.4))
+    model.add(Dropout(0.4))
     model.add(Flatten())
     model.add(Dense(128, activation='relu', kernel_regularizer=l2(args.l2),
                     kernel_initializer='he_uniform'))
