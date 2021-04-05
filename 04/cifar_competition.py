cbd3395c1b856fd1b6aef948985354be4934df56
mysak <mysak@cloudcode.cz>
2021-04-05 18:03:03 +0200

    max_capacity

--- 04/cifar_competition.py
+++ 04/cifar_competition.py
@@ -80,7 +80,7 @@ def main(args):
     # Load data
     cifar = CIFAR10()
     # TODO: Create the model and train it
-    v = 0.5
+    v = 0.1
     #model = MyModel()
     model = Sequential()
     model.add(Conv2D(32//v, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', kernel_regularizer=l2(args.l2), input_shape=(32, 32, 3)))
