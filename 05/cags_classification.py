9dc51adb8b93923536d93b7dfdc38cdce67219e5
mysak <mysak@cloudcode.cz>
2021-04-12 20:58:24 +0200

    no layers

--- 05/cags_classification.py
+++ 05/cags_classification.py
@@ -27,11 +27,11 @@ if use_neptune:
 
 # TODO: Define reasonable defaults and optionally more parameters
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=128, type=int, help="Batch size.")
+parser.add_argument("--batch_size", default=32, type=int, help="Batch size.")
 parser.add_argument("--epochs", default=100, type=int, help="Number of epochs.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-parser.add_argument("--learning_rate", default=0.05, type=int, help="Learning rate.")
+parser.add_argument("--learning_rate", default=0.01, type=int, help="Learning rate.")
 parser.add_argument("--use_lrplatau", default=False, type=int, help="Use LR decay on platau")
 
 def main(args):
@@ -60,12 +60,26 @@ def main(args):
 
     # Load the data
     cags = CAGS()
+
+
+    '''
+    a1 = list( train.take(1) )[0][0][0].numpy()
+    import cv2
+    cv2.namedWindow('now', cv2.WINDOW_NORMAL)
+    cv2.imshow("now", a1)
+    cv2.waitKey(0)
+    '''
+
+
+
+
+
     l = 2142
     '''
     train = cags.train.map(lambda example: (example["image"], example["label"])).batch(args.batch_size).take(-1).cache()
     '''
     train = cags.train.map(lambda example: (example["image"], example["label"])).take(-1).map(
-        lambda image, label: (tf.image.resize_with_crop_or_pad(image, cags.H + 200, cags.W + 200), label), num_parallel_calls=10
+        lambda image, label: (tf.image.resize_with_crop_or_pad(image, cags.H + 40, cags.W + 40), label), num_parallel_calls=10
         ).cache()
     train = train.shuffle(l).map(
             lambda image, label: (tf.image.random_flip_left_right(image), label)
@@ -84,18 +98,11 @@ def main(args):
     efficientnet_b0 = efficient_net.pretrained_efficientnet_b0(include_top=False)
     efficientnet_b0.trainable= False
     
-    x = tf.keras.layers.Dense( 1000, activation='relu' )(efficientnet_b0.output[0])
-    x = tf.keras.layers.Dense( len(cags.LABELS), activation='softmax' )(x)
+    #x = tf.keras.layers.Dense( 1000, activation='relu' )(efficientnet_b0.output[0])
+    x = tf.keras.layers.Dense( len(cags.LABELS), activation='softmax' )(efficientnet_b0.output[0])
     # TODO: Create the model and train it
     model = Model(inputs=[efficientnet_b0.input], outputs=[x] )
 
-    '''
-    a1 = list( train.take(1) )[0][0][0].numpy()
-    import cv2
-    cv2.namedWindow('now', cv2.WINDOW_NORMAL)
-    cv2.imshow("now", a1)
-    cv2.waitKey(0)
-    '''
 
     decay_steps = args.epochs * l / args.batch_size
     lr_decayed_fn = tf.keras.experimental.CosineDecay(args.learning_rate, decay_steps)
@@ -131,7 +138,7 @@ def main(args):
     for layer in model.layers[fine_tune_at:]:
         layer.trainable = True
 
-    model.compile(optimizer=tf.keras.optimizers.SGD(lr_decayed_fn, momentum=0.9, nesterov=True), 
+    model.compile(optimizer=tf.keras.optimizers.SGD(0.0001, momentum=0.9, nesterov=True), 
         loss=tf.keras.losses.SparseCategoricalCrossentropy(), 
         metrics=['SparseCategoricalAccuracy'] 
         )
