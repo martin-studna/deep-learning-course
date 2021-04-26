1429e72f031454f80c434e0806f20e9454e835c1
DESKTOP-QPROHSL\CLOUDCODE <albert.mysak@gmail.com>
2021-04-26 16:43:59 +0200

    0.13%

--- 07/svhn_competition.py
+++ 07/svhn_competition.py
@@ -29,10 +29,10 @@ parser.add_argument("--threads", default=1, type=int,
 def draw(img, bboxes):
     import cv2
     cv2.namedWindow("now", cv2.WINDOW_NORMAL)
+    im = np.array(img)
     for i in range( len(bboxes) ):
-        cv2.rectangle(  img, (int(bboxes[i][1]),int(bboxes[i][0])  ), (int(bboxes[i][3]),int(bboxes[i][2])), (255, 0, 0)  ) 
-    
-    cv2.imshow("now", img)
+        cv2.rectangle(  im, (int(bboxes[i][1]),int(bboxes[i][0])  ), (int(bboxes[i][3]),int(bboxes[i][2])), (255, 0, 0), 3  ) 
+    cv2.imshow("now", im)
     cv2.waitKey(0)
 
 
@@ -69,12 +69,19 @@ def main(args):
     #draw(t[0][0], t[0][1])
 
     my_anchors = []
+    '''
+    000:array([-0.25, -0.15,  0.25,  0.15])
+    001:array([-0.17307692, -0.15      ,  0.32692308,  0.15      ])
+    002:array([-0.09615385, -0.15      ,  0.40384615,  0.15      ])
+    003:array([-0.01923077, -0.15      ,  0.48076923,  0.15      ])
+    004:array([ 0.05769231, -0.15      ,  0.55769231,  0.15      ])
+    '''
 
-    a1 = [0.4,0.3]
-
-    for x in np.arange(0,1, 0.1):
-        for y in np.arange(0,1, 0.1):
-            my_anchors.append(  (y-a1[1], x-a1[0], y+a1[1], x+a1[0])    )
+    a1 = [0.3,0.5]
+    
+    for y in np.linspace(0,1,14):
+        for x in np.linspace(0,1,14):
+            my_anchors.append(  (y-a1[1]/2, x-a1[0]/2, y+a1[1]/2, x+a1[0]/2 )    )
 
     my_anchors = np.array(my_anchors)
     #co je anchor
@@ -83,7 +90,7 @@ def main(args):
     #kdy voláme bboxes_training
     #roi pooling
 
-    strana = 46
+    strana = 224
 
     x_train = []
 
@@ -95,7 +102,7 @@ def main(args):
         h, w, c = t[i][0].shape
         nasobek_h = strana / h 
         nasobek_w = strana / w 
-        img= cv2.resize(t[i][0], (46,46)  )
+        img= cv2.resize(t[i][0], (strana,strana)  )
         x_train.append( img )
             
         g_boxes = np.array( t[i][1] )
@@ -104,9 +111,14 @@ def main(args):
         g_boxes[:,1] *= nasobek_w / strana
         g_boxes[:,3] *= nasobek_w / strana
 
-        classes, bboxesy = bboxes_utils.bboxes_training( my_anchors, t[i][2], g_boxes , 0.2 )
+        classes, bboxesy = bboxes_utils.bboxes_training( my_anchors, t[i][2], g_boxes , 0.5 )
         #draw(img, g_boxes * strana)
+
         cat_classes = keras.utils.to_categorical(  classes, 11    )
+        if i < 10:
+            draw(img, my_anchors[ cat_classes.argmax(axis=1) > 0] * strana)
+        #draw(img, my_anchors[7+7*14:8+7*14] * strana)
+
 
         all_bboxes.append(bboxesy)
         all_cat_classes.append(cat_classes)
@@ -119,65 +131,80 @@ def main(args):
 
     x_test = []
     for i in range(len(test_list)):
-        x_test.append( cv2.resize(test_list[i], (46,46)  ) )
+        x_test.append( cv2.resize(test_list[i], (strana,strana)  ) )
     x_test = np.array(x_test)
 
-    
 
     # Load the EfficientNet-B0 model
     efficientnet_b0 = efficient_net.pretrained_efficientnet_b0(
         include_top=False)
+    efficientnet_b0.trainable = False    
 
     # TODO: Create the model and train it
-    input_l = keras.layers.Input(shape=(46,46,3))
-    o0, o1, o2, o3, o4, o5*_ = efficientnet_b0(input_l)
-    x = keras.layers.Conv2D(32, 7, activation='relu')(input_l)
-    x = keras.layers.Conv2D(32, 5, activation='relu')(x)
-    x = keras.layers.Conv2D(32, 7, strides=2, activation='relu' )(x)
-    x = keras.layers.Conv2D(32, 3, activation='relu' )(x)
-    x = keras.layers.Conv2D(32, 4, activation='relu' )(x)
+    input_l = keras.layers.Input(shape=(strana,strana,3))
+    o0, o1, o2, o3, o4, o5, *_ = efficientnet_b0(input_l)
+    x = o2
 
-    classes = keras.layers.Conv2D(11, 11, padding='same', activation='relu')(x)
-    classes = keras.layers.Reshape((100,11))(classes)
-    classes = keras.layers.Activation('softmax', name="classes_output")(classes)
+    #classes = keras.layers.Conv2D(256, 3, padding='same', activation='relu')(x)
+    #classes = keras.layers.Conv2D(256, 3, padding='same', activation='relu')(classes)
+    #classes = keras.layers.Conv2D(256, 3, padding='same', activation='relu')(classes)
+    classes = keras.layers.Conv2D(256, 3, padding='same')(x)
+    classes = keras.layers.BatchNormalization()(classes)
+    classes = keras.layers.Activation('relu')(classes)
+
+    classes = keras.layers.Conv2D(256, 3, padding='same')(classes)
+    classes = keras.layers.BatchNormalization()(classes)
+    classes = keras.layers.Activation('relu')(classes)
+
+
+    classes = keras.layers.Conv2D(10, 3, padding='same', activation='sigmoid')(classes)
+    classes = keras.layers.Reshape((14*14,10), name="classes_output" )(classes)
+    #classes = keras.layers.Activation('sigmoid')(classes)
     #classes = keras.layers.Flatten(name="classes_output")(classes)
 
 
-    bboxes = keras.layers.Conv2D(4, 11, padding='same', activation='relu')(x)
-    bboxes = keras.layers.Reshape((100,4), name="bboxes_output")(bboxes)
+    bboxes = keras.layers.Conv2D(256, 3, padding='same')(x)
+    bboxes = keras.layers.BatchNormalization()(bboxes)
+    bboxes = keras.layers.Activation('relu')(bboxes)
+
+    bboxes = keras.layers.Conv2D(256, 3, padding='same')(bboxes)
+    bboxes = keras.layers.BatchNormalization()(bboxes)
+    bboxes = keras.layers.Activation('relu')(bboxes)
+
+
+    bboxes = keras.layers.Conv2D(4, 3, padding='same')(bboxes)
+    bboxes = keras.layers.Reshape((14*14,4), name="bboxes_output")(bboxes)
 
     model = keras.models.Model(inputs=[input_l], outputs=[classes, bboxes]   )
     model.summary()
 
-    def my_loss_fn(y_true, y_pred):
-        squared_difference = tf.square(y_true - y_pred)
-        return tf.reduce_mean(squared_difference, axis=-1)  # Note the `axis=-1`
-        
     losses = {
         #'classes_output':  tfa.losses.SigmoidFocalCrossEntropy(reduction=tf.losses.Reduction.SUM_OVER_BATCH_SIZE) ,
-        'classes_output':   tfa.losses.SigmoidFocalCrossEntropy(reduction=tf.losses.Reduction.SUM_OVER_BATCH_SIZE) ,
+        'classes_output':   tfa.losses.SigmoidFocalCrossEntropy(reduction=tf.losses.Reduction.SUM_OVER_BATCH_SIZE) , #<5% = backgroud!!!???
         'bboxes_output':  tf.keras.losses.Huber(),
         } 
 
     metrics = {
-        'classes_output': keras.metrics.CategoricalAccuracy(),
+        'classes_output': keras.metrics.BinaryAccuracy(),
         } 
 
-    model.compile(optimizer=keras.optimizers.Adam(0.0001), 
+    model.compile(optimizer=keras.optimizers.Adam(), 
     loss=  losses, 
     metrics=metrics , 
-    run_eagerly=True    )
+    run_eagerly=False    )
 
+    #sample weight shape: (10000, 196)
 
-
-    model.fit( x_train,  { 'classes_output': all_cat_classes , 'bboxes_output': all_bboxes    } ,batch_size=20, epochs=10, sample_weight=[all_sample_weights,all_sample_weights] )
-    #model.fit( x_train,  { 'classes_output': all_cat_classes, 'bboxes_output': all_bboxes } ,batch_size=50, epochs=20 )
+    #model.fit( x_train,  { 'classes_output': all_cat_classes[:,:,1:] , 'bboxes_output': all_bboxes    } ,batch_size=16, epochs=1, sample_weight={ 'classes_output': np.ones_like(all_sample_weights) , 'bboxes_output': all_sample_weights    } )
+    model.fit( x_train,  { 'classes_output': all_cat_classes[:,:,1:] , 'bboxes_output': all_bboxes    } ,batch_size=16, epochs=1, sample_weight={ 'bboxes_output': all_sample_weights     } )
+    #model.fit( x_train,  { 'classes_output': all_cat_classes[:,:,1:] , 'bboxes_output': all_bboxes    } ,batch_size=2, epochs=1, sample_weight={ 'classes_output': all_sample_weights, 'bboxes_output': all_sample_weights    } )
     #https://github.com/fizyr/keras-retinanet/blob/master/keras_retinanet/bin/train.py
 
     #model.predict( np.array( [ t[0][0]] )  )[0].argmax(axis=2)  
 
     model.save('model.h5')
-    
+    ''' 
+    '''
     model = keras.models.load_model('model.h5')
 
     # Generate test set annotations, but in args.logdir to allow parallel execution.
@@ -192,24 +219,27 @@ def main(args):
     
         #for predicted_classes, predicted_bboxes in zip(tpredicted_classes, tpredicted_bboxes):
         for i in range(len(tpredicted_classes)):
-            scores = tpredicted_classes[i].max(axis=1)
-            predicted_classes = tpredicted_classes[i].argmax(axis=1)
+            scores = tpredicted_classes[i,:,1:].max(axis=1)
+            predicted_classes = tpredicted_classes[i,:,1:].argmax(axis=1)
             predicted_bboxes = tpredicted_bboxes[i] 
-            predicted_bboxess = bboxes_utils.bboxes_from_fast_rcnn( my_anchors ,predicted_bboxes) * 46
 
-            selected_indices = tf.image.non_max_suppression(predicted_bboxess, scores, 5).numpy()
+            predicted_bboxess = bboxes_utils.bboxes_from_fast_rcnn( my_anchors ,predicted_bboxes) * strana
+
+            selected_indices = tf.image.non_max_suppression(predicted_bboxess, scores, 5, iou_threshold=0.2, score_threshold=0.2).numpy()
             #selected_boxes = tf.gather(predicted_bboxess, selected_indices)
             selected_boxes = predicted_bboxess[ selected_indices]
-
+            selected_scores = scores[ selected_indices]
+            selected_predicted_classes = predicted_classes[ selected_indices]
 
             if i < 10:
+                print(selected_predicted_classes)
                 draw(x_test[i], selected_boxes)
 
             output = ""
-            for label, bbox in zip(predicted_classes, selected_boxes):
+            for label, bbox in zip(selected_predicted_classes, selected_boxes):
                 #output += [label] + bbox
-                if label != 0:
-                    output += str(label-1)+ " " + str(int(bbox[0]))+ " " +  str(int(bbox[1]))+ " " +  str(int(bbox[2]))+ " " +  str(int(bbox[3])) + " " 
+                #if label != 0:
+                output += str(label+1)+ " " + str(int(bbox[0]))+ " " +  str(int(bbox[1]))+ " " +  str(int(bbox[2]))+ " " +  str(int(bbox[3])) + " " 
             print(*output, file=predictions_file, sep='')
             #print(' '.join(output), file=predictions_file)
 
