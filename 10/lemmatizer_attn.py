8cafa57492ad165d6e782a9b9688552e7d1c91ad
martin-studna <martin.studna2@gmail.com>
2021-06-20 12:34:17 +0200

    improvements

--- 10/lemmatizer_attn.py
+++ 10/lemmatizer_attn.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
+from morpho_dataset import MorphoDataset
+import tensorflow_addons as tfa
+import tensorflow as tf
+import numpy as np
 import argparse
 import datetime
 import os
 import re
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2") # Report only TF errors by default
+# Report only TF errors by default
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")
 
-import numpy as np
-import tensorflow as tf
-import tensorflow_addons as tfa
-
-from morpho_dataset import MorphoDataset
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
-parser.add_argument("--cle_dim", default=64, type=int, help="CLE embedding dimension.")
+parser.add_argument("--cle_dim", default=64, type=int,
+                    help="CLE embedding dimension.")
 parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
-parser.add_argument("--max_sentences", default=None, type=int, help="Maximum number of sentences to load.")
-parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
-parser.add_argument("--rnn_dim", default=64, type=int, help="RNN cell dimension.")
+parser.add_argument("--max_sentences", default=None, type=int,
+                    help="Maximum number of sentences to load.")
+parser.add_argument("--recodex", default=False,
+                    action="store_true", help="Evaluation in ReCodEx.")
+parser.add_argument("--rnn_dim", default=64, type=int,
+                    help="RNN cell dimension.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--threads", default=1, type=int,
+                    help="Maximum number of threads to use.")
 # If you add more arguments, ReCodEx will keep them with your default values.
+
 
 class Network(tf.keras.Model):
     def __init__(self, args, train):
@@ -56,8 +62,10 @@ class Network(tf.keras.Model):
             metrics=[tf.metrics.Accuracy(name="accuracy")],
         )
 
-        self.tb_callback = tf.keras.callbacks.TensorBoard(args.logdir, update_freq=100, profile_batch=0)
-        self.tb_callback._close_writers = lambda: None # A hack allowing to keep the writers open.
+        self.tb_callback = tf.keras.callbacks.TensorBoard(
+            args.logdir, update_freq=100, profile_batch=0)
+        # A hack allowing to keep the writers open.
+        self.tb_callback._close_writers = lambda: None
 
     class DecoderTraining(tfa.seq2seq.BaseDecoder):
         def __init__(self, lemmatizer, *args, **kwargs):
@@ -68,11 +76,13 @@ class Network(tf.keras.Model):
         def batch_size(self):
             # TODO(lemmatizer_noattn): Return the batch size of self.source_states, using tf.shape
             raise NotImplementedError()
+
         @property
         def output_size(self):
             # TODO(lemmatizer_noattn): Return `tf.TensorShape(number of logits per each output element)`
             # By output element we mean characters.
             raise NotImplementedError()
+
         @property
         def output_dtype(self):
             # TODO(lemmatizer_noattn): Return the type of the logits
@@ -132,6 +142,7 @@ class Network(tf.keras.Model):
             # TODO(lemmatizer_noattn): Return `tf.TensorShape()` describing a scalar element,
             # because we are generating scalar predictions now.
             raise NotImplementedError()
+
         @property
         def output_dtype(self):
             # TODO(lemmatizer_noattn): Return the type of the generated predictions
@@ -208,19 +219,22 @@ class Network(tf.keras.Model):
         # Convert `y` by splitting characters, mapping characters to ids using
         # `self.target_mapping` and finally appending `MorphoDataset.Factor.EOW`
         # to every sequence.
-        y_targets = self.target_mapping(tf.strings.unicode_split(y.values, "UTF-8"))
+        y_targets = self.target_mapping(
+            tf.strings.unicode_split(y.values, "UTF-8"))
         y_targets = tf.concat(
             [y_targets, tf.fill([y_targets.bounding_shape(0), 1], tf.constant(MorphoDataset.Factor.EOW, tf.int64))], axis=-1)
         y_targets = y.with_values(y_targets)
 
         with tf.GradientTape() as tape:
             y_pred = self(x, targets=y_targets, training=True)
-            loss = self.compiled_loss(y_targets.flat_values, y_pred.flat_values, regularization_losses=self.losses)
+            loss = self.compiled_loss(
+                y_targets.flat_values, y_pred.flat_values, regularization_losses=self.losses)
         self.optimizer.minimize(loss, self.trainable_variables, tape=tape)
         return {"loss": loss}
 
     def predict_step(self, data):
-        if isinstance(data, tuple): data = data[0]
+        if isinstance(data, tuple):
+            data = data[0]
         y_pred = self(data, training=False)
         y_pred = self.target_mapping_inverse(y_pred)
         y_pred = tf.strings.reduce_join(y_pred, axis=-1)
@@ -229,8 +243,10 @@ class Network(tf.keras.Model):
     def test_step(self, data):
         x, y = data
         y_pred = self.predict_step(data)
-        self.compiled_metrics.update_state(tf.ones_like(y.values, dtype=tf.int32), tf.cast(y_pred.values == y.values, tf.int32))
+        self.compiled_metrics.update_state(tf.ones_like(
+            y.values, dtype=tf.int32), tf.cast(y_pred.values == y.values, tf.int32))
         return {m.name: m.result() for m in self.metrics}
+
 
 def main(args):
     # Fix random seeds and threads
@@ -239,19 +255,24 @@ def main(args):
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
     if args.recodex:
-        tf.keras.utils.get_custom_objects()["glorot_uniform"] = tf.initializers.GlorotUniform(seed=args.seed)
-        tf.keras.utils.get_custom_objects()["orthogonal"] = tf.initializers.Orthogonal(seed=args.seed)
-        tf.keras.utils.get_custom_objects()["uniform"] = tf.initializers.RandomUniform(seed=args.seed)
+        tf.keras.utils.get_custom_objects(
+        )["glorot_uniform"] = tf.initializers.GlorotUniform(seed=args.seed)
+        tf.keras.utils.get_custom_objects(
+        )["orthogonal"] = tf.initializers.Orthogonal(seed=args.seed)
+        tf.keras.utils.get_custom_objects(
+        )["uniform"] = tf.initializers.RandomUniform(seed=args.seed)
 
     # Create logdir name
     args.logdir = os.path.join("logs", "{}-{}-{}".format(
         os.path.basename(globals().get("__file__", "notebook")),
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+        ",".join(("{}={}".format(re.sub(
+            "(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
 
     # Load the data
-    morpho = MorphoDataset("czech_cac", max_sentences=args.max_sentences, add_bow_eow=True)
+    morpho = MorphoDataset(
+        "czech_cac", max_sentences=args.max_sentences, add_bow_eow=True)
 
     # Create the network and train
     network = Network(args, morpho.train)
@@ -260,28 +281,35 @@ def main(args):
     def create_dataset(name):
         dataset = getattr(morpho, name).dataset
         dataset = dataset.map(lambda forms, lemmas, tags: (forms, lemmas))
-        dataset = dataset.shuffle(len(dataset), seed=args.seed) if name == "train" else dataset
-        dataset = dataset.apply(tf.data.experimental.dense_to_ragged_batch(args.batch_size))
+        dataset = dataset.shuffle(
+            len(dataset), seed=args.seed) if name == "train" else dataset
+        dataset = dataset.apply(
+            tf.data.experimental.dense_to_ragged_batch(args.batch_size))
         return dataset
-    train, dev, test = create_dataset("train"), create_dataset("dev"), create_dataset("test")
+    train, dev, test = create_dataset(
+        "train"), create_dataset("dev"), create_dataset("test")
 
     # Callback showing intermediate results during training
     class ShowIntermediateResults(tf.keras.callbacks.Callback):
         def __init__(self, data):
             self._iterator = iter(data.repeat())
+
         def on_train_batch_end(self, batch, logs=None):
             if network.optimizer.iterations % 10 == 0:
                 forms, lemmas = next(self._iterator)
-                tf.print(network.optimizer.iterations, forms[0, 0], lemmas[0, 0], network.predict_on_batch(forms[:1, :1])[0, 0])
+                tf.print(network.optimizer.iterations, forms[0, 0], lemmas[0, 0], network.predict_on_batch(
+                    forms[:1, :1])[0, 0])
 
     network.fit(train, epochs=args.epochs, validation_data=dev, verbose=2,
                 callbacks=[ShowIntermediateResults(dev), network.tb_callback])
 
     test_logs = network.evaluate(test, return_dict=True)
-    network.tb_callback.on_epoch_end(args.epochs, {"val_test_" + metric: value for metric, value in test_logs.items()})
+    network.tb_callback.on_epoch_end(
+        args.epochs, {"val_test_" + metric: value for metric, value in test_logs.items()})
 
     # Return test set accuracy for ReCodEx to validate
     return test_logs["accuracy"]
+
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
